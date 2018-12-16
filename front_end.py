1b5636a3c38fc95bf869255467770fa21aed7a74
eddyizm hplaptop <eddyizm@me.com>
2018-12-16 15:52:36 -0800

    added button

--- front_end.py
+++ front_end.py
@@ -1,6 +1,7 @@
 from appJar import gui
 
 win = gui('Hello World')
+# widgets
 win.addLabel('label1', 'hello world')
 win.addLabel('label2', 'python appJar test')
 win.setBg('green')
@@ -9,4 +10,9 @@ win.setLabelFg('label2', 'yellow')
 win.setFont(20)
 win.setSize('400x200')
 win.setResizable(False)
+# buttons
+def press(name):
+    print (name, 'button pressed')
+
+win.addButton('Press me', press)
 win.go()
