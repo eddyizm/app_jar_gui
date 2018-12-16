eac14e4dcabb8c9232281b07eb134ad315dcd087
eddyizm hplaptop <eddyizm@me.com>
2018-12-16 11:38:07 -0800

    added more widget options

--- front_end.py
+++ front_end.py
@@ -2,6 +2,11 @@ from appJar import gui
 
 win = gui('Hello World')
 win.addLabel('label1', 'hello world')
+win.addLabel('label2', 'python appJar test')
 win.setBg('green')
+win.setLabelBg('label2', 'blue')
+win.setLabelFg('label2', 'yellow')
 win.setFont(20)
+win.setSize('400x200')
+win.setResizable(False)
 win.go()
