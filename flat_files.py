fe42f251944a205a5a1aa5e535d9313abd2e55dc
Eduardo Cervantes <eddyizm@gmail.com>
2018-12-18 14:04:51 -0800

    capture values for ext, sep, and file name. checks for selections

--- flat_files.py
+++ flat_files.py
@@ -3,17 +3,27 @@ front end for extracting flat files from sql scripts
 '''
 from appJar import gui
 #funtions
-def press(name):
+def generate_file(name):
   print (name)
   print ('extension:')
   print (win.getRadioButton('f_ext'))
-  print ('options:')
-  print (win.getOptionBox('file seperator')) 
+  
+  if not win.getEntry('f1'):
+    print ('sql file empty?')
+  else:
+    print ('sql file:')
+    print (win.getEntry('f1'))
 
+  if win.getOptionBox('file seperator'):
+    print ('options:')
+    print (win.getOptionBox('file seperator')) 
+  else:
+    win.popUp('Error', 'please select a file seperator', kind='error')
+    
 win = gui(title='File exporter')
 win.setIcon('images/Extract.ico')
 win.addLabel('tx_1', 'select sql script')
-win.addFileEntry("f1")
+win.addFileEntry('f1')
 #win.addStatusbar(header='file extension selected: ')
 FILE_SEP = ['- select file seperator -', 'comma ( , )', 'tab ( )', 'pipe ( | )']
 win.addOptionBox('file seperator', FILE_SEP)
@@ -22,6 +32,6 @@ with win.labelFrame('file extension'):
   win.addRadioButton('f_ext', '.csv')
   win.addRadioButton('f_ext', '.txt')
   
-win.addButton('generate file', press)
+win.addButton('generate file', generate_file)
 
 win.go()
